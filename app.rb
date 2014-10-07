7f02ec7b3c8db580fb828c34827d369ca0017421
K-2052 <k@2052.me>
2014-10-07 16:23:07 -0700

    remove project form stuff

--- app.rb
+++ app.rb
@@ -10,22 +10,4 @@ class App < Scorched::Controller
   get '/' do
   	render :main
   end
-
-  get '/quote' do
-  	render :project_form
-  end
-
-  post '/quote' do
-  	halt "Bad Bot!" unless params[:spam].blank?
-
-    @project = params[:project]
-
-		RestClient.post "https://api:key-#{ENV['MAILGUN_KEY']}@api.mailgun.net/v2/samples.mailgun.org/messages",
-			  :from    => params[:project][:email],
-			  :to      => ENV['EMAIL_TO'],
-			  :subject => "I want a backend in a week!",
-			  :text    => render(:project_email_template)
-
-  	render :thanks
-  end
 end
